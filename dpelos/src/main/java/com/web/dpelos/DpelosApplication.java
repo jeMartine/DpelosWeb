05a5024ca1f0be85115a6907c8bc25b7c45ab212
jeMartine <jorgeesteban464@gmail.com>
2024-08-11 19:01:46 -0500

    implementar index springboot

---
+++ dpelos/src/main/java/com/web/dpelos/DpelosApplication.java
@@ -0,0 +1,13 @@
+package com.web.dpelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpelosApplication.class, args);
+	}
+
+}
---
+++ dpelos/src/test/java/com/web/dpelos/DpelosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.dpelos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpelosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
