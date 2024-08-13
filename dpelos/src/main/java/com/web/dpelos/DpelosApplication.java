4e340fcc96bf6150ad36bc73106a2a9fffb75a38
diego4lbarracin <diegoalbarracin0405@gmail.com>
2024-08-12 22:10:39 -0500

    Añadiendo carpetas y un controlador para las peticiones de la lista de mascotas.

--- dpelos/src/main/java/com/web/dpelos/DpelosApplication.java
+++ dpelos/src/main/java/com/web/dpelos/DpelosApplication.java
@@ -1,9 +1,15 @@
 package com.web.dpelos;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration //Beans
+@EnableAutoConfiguration //Activar auto configuracion
+@ComponentScan //Escanear la info.
 public class DpelosApplication {
 
 	public static void main(String[] args) {
