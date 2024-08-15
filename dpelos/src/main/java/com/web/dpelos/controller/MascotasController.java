0b262bc69aa0d17919191454801525bdc6d618b5
jeMartine <jorgeesteban464@gmail.com>
2024-08-15 14:24:18 -0500

    Update: tamaño logo

--- dpelos/src/main/java/com/web/dpelos/controller/MascotasController.java
+++ dpelos/src/main/java/com/web/dpelos/controller/MascotasController.java
@@ -1,19 +1,14 @@
 package com.web.dpelos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.web.dpelos.entity.Mascota;
-import com.web.dpelos.service.MascotaService;
 import com.web.dpelos.service.MascotaServiceImplementation;
 
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
