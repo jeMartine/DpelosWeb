c69ec984ff7adeb330da03b0ed8ea37177234252
majo425 <mariacardenasm@javeriana.edu.co>
2024-08-31 18:14:52 -0500

    Update login y dueño

--- dpelos/src/main/resources/static/js/duenoScript.js
+++ dpelos/src/main/resources/static/js/duenoScript.js
@@ -1,4 +1,6 @@
-import { Input, initMDB } from "mdb-ui-kit";
+/*import { Input, initMDB } from "mdb-ui-kit";
+
+
 
 initMDB({ Input });
 
@@ -7,4 +9,63 @@ const searchInput = document.getElementById('search-input');
 searchButton.addEventListener('click', () => {
   const inputValue = searchInput.value;
   alert(inputValue);
+});*/
+
+$(document).ready(function() {
+  const $owl = $(".owl-carousel");
+
+  $owl.owlCarousel({
+    items: 3,
+    margin: 10,
+    loop: false,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+
+  function adjustCarouselForSingleItem() {
+    const itemsCount = $owl.find(".owl-item").length;
+
+    if (itemsCount === 1) {
+      $owl.trigger('add.owl.carousel', ['<div class="item empty-item"></div>', 0]);
+      $owl.trigger('refresh.owl.carousel');
+      $owl.trigger('to.owl.carousel', [1, 0, true]);
+    } else if (itemsCount === 2) {
+      $owl.trigger('refresh.owl.carousel');
+    }
+  }
+
+  adjustCarouselForSingleItem();
+
+  $(".owl-carousel").on("click", ".img-mascota", function() {
+    $(".card-mascota").removeClass("selected");
+    $("#info-mascota").hide();
+
+    $(this).closest(".card-mascota").addClass("selected");
+
+    const nombre = $(this).siblings(".img-caption").find("p").text();
+    const fecha = $(this).data("fecha");
+    const estado = $(this).data("estado");
+    const enfermedad = $(this).data("enfermedad");
+
+    $("#info-nombre").text(nombre);
+    $("#info-fecha").text(fecha);
+    $("#info-estado").text(estado);
+    $("#info-enfermedad").text(enfermedad);
+
+    $("#info-mascota").show();
+  });
+
+  $(window).resize(function() {
+    adjustCarouselForSingleItem();
+  });
 });
--- dpelos/src/main/resources/static/js/loginScript.js
+++ dpelos/src/main/resources/static/js/loginScript.js
@@ -2,6 +2,7 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
+
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
     document.body.style.backgroundColor = '#94F3D4'; // Color del empleado
@@ -14,3 +15,15 @@ loginBtn.addEventListener('click', () => {
     document.body.style.backgroundColor = '#fbdb5d'; // Color del cliente
     document.body.style.transition="all 0.6s ease-in-out";
 });
+
+$(document).ready(function () {
+    if (window.location.search.includes("error=true")) {
+        $("#modalCliente").modal("show");
+    }
+    $('#modalCliente').on('hidden.bs.modal', function () {
+        window.location.href = '/login';
+    });
+});
+
+
+
