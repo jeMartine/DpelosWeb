23bf827bac253898a310035d95b5c4a82e7cd1f9
diego4lbarracin <diegoalbarracin0405@gmail.com>
2024-09-26 08:43:27 -0500

    Feature: Adding Tratamientos.

--- dpelos/src/main/java/com/web/dpelos/entity/Tratamiento.java
+++ dpelos/src/main/java/com/web/dpelos/entity/Tratamiento.java
@@ -4,6 +4,7 @@ import java.sql.Date;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -17,8 +18,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Tratamiento {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idTratamiento;
+
     private Date fechaAdministracion;
     private String descripcionTratamiento;
 
@@ -34,7 +36,12 @@ public class Tratamiento {
     @JoinColumn(name = "veterinario_id")
     private Veterinario veterinario;
 
-    public Tratamiento(Date fechaAdministracion, Droga droga, Mascota mascota, Veterinario veterinario, String descripcionTratamiento) {
+    // public Tratamiento(Date fechaAdministracion, Droga droga, Mascota mascota,
+    // Veterinario veterinario, String descripcionTratamiento) {
+    // this.fechaAdministracion = fechaAdministracion;
+    // this.descripcionTratamiento = descripcionTratamiento;
+    // }
+    public Tratamiento(Date fechaAdministracion, String descripcionTratamiento) {
         this.fechaAdministracion = fechaAdministracion;
         this.descripcionTratamiento = descripcionTratamiento;
     }
