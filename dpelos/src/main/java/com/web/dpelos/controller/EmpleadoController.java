ae97c28b0e0c974d96981a5a723cf52203229cdf
jeMartine <jorgeesteban464@gmail.com>
2024-10-06 14:38:18 -0500

    Login empleado

--- dpelos/src/main/java/com/web/dpelos/controller/EmpleadoController.java
+++ dpelos/src/main/java/com/web/dpelos/controller/EmpleadoController.java
@@ -1,8 +1,6 @@
 package com.web.dpelos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- dpelos/src/main/java/com/web/dpelos/controller/EnfermedadController.java
+++ dpelos/src/main/java/com/web/dpelos/controller/EnfermedadController.java
@@ -3,26 +3,18 @@ package com.web.dpelos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.web.dpelos.entity.Enfermedad;
-import com.web.dpelos.entity.Veterinario;
 import com.web.dpelos.service.EnfermedadService;
-import com.web.dpelos.service.VeterinarioService;
-
-import jakarta.servlet.http.HttpSession;
 
 @RestController // Esta anotacion indica que esta clase es un controlador REST
 @RequestMapping("/enfermedad") // Esta anotacion indica la URL base para todos los metodos de esta clase
