3accf9a6bfbaf65cd2e96140960496fe433b2ad4
jeMartine <jorgeesteban464@gmail.com>
2024-10-10 12:50:46 -0500

    merge main

--- dpelos/src/main/java/com/web/dpelos/controller/indexController.java
+++ dpelos/src/main/java/com/web/dpelos/controller/indexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping()
-public class IndexController {
+public class indexController {
     public String vistaPrincipal() {
         return "index";
     }
