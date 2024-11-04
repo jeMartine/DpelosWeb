c62b720e162016962102a88ae2fb22f4bdc6a376
majo425 <mariacardenasm@javeriana.edu.co>
2024-11-04 12:01:28 -0500

    Security 2.0

--- dpelos/src/main/java/com/web/security/SecurityConfig.java
+++ dpelos/src/main/java/com/web/security/SecurityConfig.java
@@ -8,20 +8,20 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
-            /* H2 */
-            .headers(headers -> headers.frameOptions(frame -> frame.disable()))
-            .authorizeHttpRequests(requests -> requests
-            .requestMatchers("/h2/**").permitAll()
-            .anyRequest().permitAll());
+                .csrf(AbstractHttpConfigurer::disable)
+                /* H2 */
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+                .authorizeHttpRequests(requests -> requests
+                        /* H2 */
+                        .requestMatchers("/h2/**").permitAll()
+                        .anyRequest().permitAll()
+                );
         return http.build();
     }
-
 }
